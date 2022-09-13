aad38b639ecf81423fe027abb9334d03872e7c96
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2022-09-13 16:48:10 +0600

    Create I.cpp

---
+++ RUET_IUPC_2022/I.cpp
@@ -0,0 +1,94 @@
+// Created by ash_98
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define mx 205
+#define ll long long
+#define mod 1000000007 //998244353
+ 
+bool dp[mx][2005][mx];
+int vis[mx][2005][mx];
+char ch[mx];
+int n,m,ii,k;
+
+vector<vector<bool>>digit={{1,1,1,1,1,1,0},{0,1,1,0,0,0,0},{1,1,0,1,1,0,1},{1,1,1,1,0,0,1},
+{0,1,1,0,0,1,1},{1,0,1,1,0,1,1},{1,0,1,1,1,1,1},{1,1,1,0,0,0,0},{1,1,1,1,1,1,1},{1,1,1,1,0,1,1}};
+
+bool func(int pos,int stick,int move){
+    if(move<0)return false;
+    if(pos==n){
+        return stick==1000;
+    }
+    if(vis[pos][stick][move]==ii)return dp[pos][stick][move];
+    vis[pos][stick][move]=ii;
+    bool re=false;
+    for(int i=9;i>=0;i--){
+        int need_mov=0;
+        int val=ch[pos]-'0';
+        int need=0;
+
+        for(int j=0;j<7;j++){
+            if(digit[val][j]==digit[i][j])continue;
+            need+=digit[i][j];
+            need-=digit[val][j];
+            need_mov++;
+        }
+        re|=func(pos+1,stick+need,move-need_mov);
+        if(re)return dp[pos][stick][move]=true;
+    }
+    return dp[pos][stick][move]=false;
+}
+ 
+void solve()
+{
+    scanf("%s%d",ch,&m);
+    n=strlen(ch);
+    printf("Case %d:\n",++ii);
+    while(m--){
+       scanf("%d",&k);
+       func(0,1000,k+k);
+
+       int stick=1000;
+       int move=k+k;
+
+       for(int pos=0;pos<n;pos++){
+
+          int val=ch[pos]-'0';
+
+          for(int i=9;i>=0;i--){
+
+            int need_mov=0;
+            int need=0;
+
+            for(int j=0;j<7;j++){
+                if(digit[val][j]==digit[i][j])continue;
+                need+=digit[i][j];
+                need-=digit[val][j];
+                need_mov++;
+            }
+
+            if(func(pos+1,stick+need,move-need_mov)){
+                printf("%d",i);
+                stick+=need;
+                move-=need_mov;
+                break;
+            }
+
+          }
+       }
+       printf("\n");
+    }
+
+    
+
+ 
+}
+ 
+int main()
+{
+    int t=1;
+    scanf("%d",&t);
+    while(t--)solve();
+    return 0;
+}
