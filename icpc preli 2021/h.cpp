8216b3539880c6c03a4ef14b79c7b4af48e9b491
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2023-01-18 22:28:17 +0600

    Update h.cpp

--- icpc preli 2021/h.cpp
+++ icpc preli 2021/h.cpp
@@ -13,25 +13,32 @@ int n,m,ii,k;
 namespace Segment_Tree
 {
     const int N=200005;
-    int Tree[2][N*4][27];
-    int Lazy[2][N*4][27];
+    vector<int>Tree[2][N*4];
+    vector<int>dummy(26);
+    int Lazy[2][N*4];
 
-    void Relax(bool fos,int ti,int node,int be,int en)
+    void Relax(bool fos,int node,int be,int en)
     {
-        if(Lazy[fos][node][ti]==-1)return;
+        if(Lazy[fos][node]==-1)return;
 
-        Tree[fos][node][ti]=Lazy[fos][node][ti]*(en-be+1);
+        int ti=Lazy[fos][node];
+
+        for(int i=0;i<26;i++)Tree[fos][node][i]=0;
+
+        Tree[fos][node][ti]=(en-be+1);
         if(be!=en)
         {
-           Lazy[fos][node*2][ti]=Lazy[fos][node][ti];
-           Lazy[fos][node*2+1][ti]=Lazy[fos][node][ti];
+           Lazy[fos][node*2]=Lazy[fos][node];
+           Lazy[fos][node*2+1]=Lazy[fos][node];
         }
-        Lazy[fos][node][ti]=-1;
+        Lazy[fos][node]=-1;
     }
 
     void init(int fos,int node,int be,int en)
     {
-        for(int i=0;i<26;i++)Lazy[fos][node][i]=-1;
+
+        Lazy[fos][node]=-1;
+        Tree[fos][node].resize(26);
         if(be==en)
         {
             for(int i=0;i<26;i++)Tree[fos][node][i]=0;
@@ -46,29 +53,33 @@ namespace Segment_Tree
     }
 
 
-    void Rupdate(int fos,int ti,int node,int be,int en,int i,int j,int val)
+    void Rupdate(int fos,int node,int be,int en,int i,int j,int val)
     {
-        Relax(fos,ti,node,be,en);
+        Relax(fos,node,be,en);
         if(be>j || en<i)return ;
         if(be>=i && en<=j)
         {
-            Lazy[fos][node][ti]=val;
-            Relax(fos,ti,node,be,en);
+            Lazy[fos][node]=val;
+            Relax(fos,node,be,en);
             return;
         }
         int mid=(be+en)/2;
-        Rupdate(fos,ti,node*2,be,mid,i,j,val);
-        Rupdate(fos,ti,node*2+1,mid+1,en,i,j,val);
-        Tree[fos][node][ti]=Tree[fos][node*2][ti]+Tree[fos][node*2+1][ti];
+        Rupdate(fos,node*2,be,mid,i,j,val);
+        Rupdate(fos,node*2+1,mid+1,en,i,j,val);
+        for(int i=0;i<26;i++)
+           Tree[fos][node][i]=Tree[fos][node*2][i]+Tree[fos][node*2+1][i];
     }
 
-    int query(int fos,int ti,int node,int be,int en,int i,int j)
+    vector<int> query(int fos,int node,int be,int en,int i,int j)
     {
-        Relax(fos,ti,node,be,en);
-        if(be>j || en<i)return 0;
-        if(be>=i && en<=j)return Tree[fos][node][ti];
+        Relax(fos,node,be,en);
+        if(be>j || en<i)return dummy;
+        if(be>=i && en<=j)return Tree[fos][node];
         int mid=(be+en)/2;
-        return query(fos,ti,node*2,be,mid,i,j)+query(fos,ti,node*2+1,mid+1,en,i,j);
+        auto a=query(fos,node*2,be,mid,i,j);
+        auto b=query(fos,node*2+1,mid+1,en,i,j);
+        for(int i=0;i<26;i++)a[i]+=b[i];
+        return a;
     }
 }
 using namespace Segment_Tree;
@@ -78,38 +89,41 @@ void solve()
     scanf("%s%s",ch[0],ch[1]);
     n=strlen(ch[0]);
     m=strlen(ch[1]);
+
     init(0,1,1,n);
     init(1,1,1,m);
+
     int q;
+
     scanf("%d",&q);
     while(q--){
         int ti,l1,l2,r1,r2;
         char c;
         scanf("%d",&ti);
+
         if(ti==1){
             scanf("%d%d %c",&l1,&r1,&c);
-            for(int i=0;i<26;i++){
-                Rupdate(0,i,1,1,n,l1,r1,0);
-            }
-            Rupdate(0,(c-'a'),1,1,n,l1,r1,1);
+            
+            Rupdate(0,1,1,n,l1,r1,c-'a');
         }
+
         else if(ti==2){
             scanf("%d%d %c",&l2,&r2,&c);
-            for(int i=0;i<26;i++){
-                Rupdate(1,i,1,1,m,l2,r2,0);
-            }
-            Rupdate(1,(c-'a'),1,1,m,l2,r2,1);
+
+            Rupdate(1,1,1,m,l2,r2,c-'a');
         }
         else
         {
             scanf("%d%d%d%d",&l1,&r1,&l2,&r2);
-            ll len1=r1-l1+1;
-            ll len2=r2-l2+1;
-            ll re=(len2+1)*(len1+1);
+            
+            ll re=(ll)(r1-l1+2)*(r2-l2+2);
+           // cout<<re<<endl;
+
+            auto a=query(0,1,1,n,l1,r1);
+            auto b=query(1,1,1,m,l2,r2);
+
             for(int i=0;i<26;i++){
-                ll from1=query(0,i,1,1,n,l1,r1);
-                ll from2=query(1,i,1,1,m,l2,r2);
-                re-=from1*from2;
+                re-=(ll)a[i]*b[i];
             }
             printf("%lld\n",re );
         }
